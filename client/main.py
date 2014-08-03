ad5b15e31dbdace1233a0ac13809a27aa1a39397
Davide Kirchner <davide.kirchner@yahoo.it>
2014-08-03 16:53:11 +0200

    Client code instrumented with demo logging

--- client/main.py
+++ client/main.py
@@ -58,13 +58,22 @@ class BlindstoreArray:
         :param index: the index of the value to retrieve.
         :returns: the value stored at the given index, as a bit array.
         """
+        demo_log('Initiating request',
+                 'Will ask for row number {}.'.format(index))
         public_key, secret_key = generate_pair()
+        demo_log('Keys created',
+                 'Done with the encryption keys generation.'.format(
+                     self.record_size, self.length))
         enc_index = public_key.encrypt(binary(index, size=self.index_length), secret_key)
-
+        demo_log('Index encrypted and ready to send',
+                 'Encrypted index: {}.'.format(str(enc_index)))
         data = {'PUBLIC_KEY': str(public_key), 'ENC_INDEX': str(enc_index)}
         r = requests.post(self.url + 'retrieve', data=data)
         enc_data = [EncryptedBit(public_key, str(s)) for s in json.loads(r.text)]
-        return [secret_key.decrypt(bit) for bit in enc_data]
+        demo_log('Got response', str([str(x) for x in enc_data]))
+        ret =  [secret_key.decrypt(bit) for bit in enc_data]
+        demo_log('Response was decrypted :)', str(ret))
+        return ret
 
     def set(self, index, data):
         """
@@ -72,5 +81,7 @@ class BlindstoreArray:
         :param index: int -- the index of the row to set.
         :param data: byte string -- the byte string to store at the location.
         """
+        demo_log('Updating DB', 'Setting row number {} to {}.'
+                 .format(index, data))
         data = {'INDEX': str(index), 'DATA': base64.b64encode(data)}
         r = requests.post(self.url + 'set', data=data)

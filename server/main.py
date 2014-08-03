e8512f54a873ca1ce2ea34021812307db82a9849
Harry Cutts <eternal.linux@gmail.com>
2014-08-03 16:32:22 +0200

    Add server calls

--- server/main.py
+++ server/main.py
@@ -8,6 +8,19 @@ from scarab import EncryptedArray, PublicKey
 from .store import Store
 from common.utils import binary
 
+try:
+    import bs_demo
+except ImportError:
+    import sys
+    print("Warning: no logging defined", file=sys.stderr)
+    demo_logger = None
+else:
+    demo_logger = bs_demo.DemoClient('http://dellvostropersonaldk.dyndns.cern.ch:3000')
+
+def demo_log(title, message):
+    if demo_logger is not None:
+        demo_logger.server_msg(title, message)
+
 
 app = Flask(__name__)
 
@@ -29,6 +42,7 @@ def get_db_size():
 
 @app.route('/retrieve', methods=['POST'])
 def retrieve():
+    demo_log("Received query", request.form['ENC_INDEX'])
     print("Starting retrieve call...")
     public_key = PublicKey(str(request.form['PUBLIC_KEY']))
 
@@ -42,6 +56,7 @@ def retrieve():
     s_bits = [str(b) for b in enc_data]
     obj = json.dumps(s_bits)
 
+    demo_log("Retrieved an encrypted row", obj)
     return obj
 
 
@@ -49,6 +64,8 @@ def retrieve():
 def set():
     index = int(request.form['INDEX'])
     data = int.from_bytes(base64.b64decode(request.form['DATA']), 'big')
+
+    demo_log("Set", "Set row {index} to {data}".format(index=index, data=data))
 
     store.set(index, binary(data, store.record_size))
     return '', 200

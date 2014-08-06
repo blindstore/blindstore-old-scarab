1f1aff8f2a279fca16769661fe2a1d4cb2f045f3
Michał Jabczyński <michal@jabczynski.net>
2014-08-06 18:40:55 +0200

    Changed the benchmarked db size

--- benchmarks/benchmark_store_retrieve.py
+++ benchmarks/benchmark_store_retrieve.py
@@ -2,12 +2,9 @@ from scarab import generate_pair
 from benchmarks.benchmark import benchmark
 from common.utils import binary
 from server import Store
-import numpy as np
 
-store = Store(database=np.array([[1, 1, 1, 1],
-                                 [1, 1, 1, 0],
-                                 [1, 1, 0, 0],
-                                 [1, 0, 0, 0]]))
+
+store = Store(record_size=20, record_count=20)
 index = 2
 pk, sk = generate_pair()
 eq = pk.encrypt(binary(index, size=store.index_length), sk)
@@ -16,4 +13,4 @@ eq = pk.encrypt(binary(index, size=store.index_length), sk)
 def func():
     list(store.retrieve(eq, pk))
 
-benchmark(func, 100, verbose=True)
+benchmark(func, 10, verbose=True)
